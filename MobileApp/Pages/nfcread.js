fdb42a32d47fd2dc25cec03f391de08cc098b430
DanielNabil74 <DanyNabil74@gmail.com>
2024-01-29 14:00:07 +0200

    nfc

---
+++ MobileApp/Pages/nfcread.js
@@ -0,0 +1,82 @@
+
+import React,{useState} from 'react'
+import { StyleSheet, ImageBackground, Image, Button, View, Pressable, Text, TextInput} from 'react-native';
+
+export default function Search ({ navigation }) {
+  const [username, setusername] = useState(null);
+  return (
+    <>
+      <View style={styles.container}>
+
+
+        <View style={styles.searchContainer}>
+            <TextInput
+            style={styles.input}
+            onChangeText={Search}
+            placeholder={'Enter username'}
+            value={username}
+            />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={styles.button}>
+            <Text style={[styles.buttontext, {fontSize: 18,fontWeight: 'bold'}]}> Search </Text>
+          </Pressable>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#121212',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    height: 200,
+    width: 300,
+    alignSelf: 'center',
+    flex: 1,
+    alignItems: 'center',
+    //flexDirection: 'column-reverse',
+  },
+  buttontext: {
+    color: 'black',  
+    textAlign: 'center',
+  },
+  button: {
+    
+    height: 50,
+    width: 150,
+    padding: 10,
+    borderRadius: 300,
+    margin: 10,
+    color:"#E2C07C",
+    backgroundColor: '#E2C07C',
+    justifyContent: 'center',
+    textAlign: 'center',
+    
+  },
+
+  searchContainer:{
+    backgroundColor: 'white',
+    width: '90%',
+    height: '6%',
+    verticalAlign: 'top',
+    marginTop: '15%',
+    borderRadius: 50,
+    textAlignVertical: 'center',
+  },
+  input:{
+    textAlignVertical:'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    flex: 1,
+
+  },
+});
